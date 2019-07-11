666248da435c7e9091b4d0ed84160f6d6f7e6696
Adrian Cisneros <cisneros_adrian51@yahoo.com>
2019-07-11 21:20:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,16 +11,18 @@ function logWhisper(string) {
   return console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  if (string = 'I love you, Grandma.') {
-    return console.log(`I love you, too.`)
+
   } 
-  else if (string.toLowerCase()){
+  if (string.toLowerCase()){
     return console.log(`I can't hear you!`)
   }
   else if (string.toUpperCase()) {
     return console.log (`YES INDEED!`)
   }
-    }
+  else if (string = 'I love you, Grandma.') {
+    return console.log(`I love you, too.`)
+   }
+   }
 sayHiToGrandma('HELLO')
 sayHiToGrandma('hello')
 sayHiToGrandma('I love you, Grandma.')
